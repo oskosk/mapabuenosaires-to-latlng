39f098ad2abc307a771c1eb2bd33cc311ee9b788
osk <oskosk@gmail.com>
2014-10-23 09:03:24 -0300

    changed dist name

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,6 @@ var concat = require('gulp-concat');
 
 gulp.task('default', function() {
   gulp.src(['./src/proj4.js', './src/mapabuenosaires-to-latlng.js'])
-    .pipe(concat('mapabuenosaires-to-latlon.js'))
+    .pipe(concat('mapabuenosaires-to-latlng.js'))
     .pipe(gulp.dest('./dist/'))
 });
